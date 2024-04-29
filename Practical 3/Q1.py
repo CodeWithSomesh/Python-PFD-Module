82fcbca6321dbc99552de7ad9275be0383dd43bc
CodeWithSomesh <codewithsomesh@gmail.com>
2024-04-29 15:55:40 +0800

    Done with Practical 3

--- Practical 3/Q1.py
+++ Practical 3/Q1.py
@@ -32,7 +32,6 @@ class BST:
             else:
                 return False
 
-        return False
 
     # this functions adds a node based on the principles of BST,
     # left nodes values are smaller than root node, vice versa
@@ -79,47 +78,62 @@ class BST:
         return elements
 
     # Using VLR to return an array of numbers/elements
-    def preOrderTraversal(self):
+    def preOrderTraversal(self, node):
         #Initialize empty array
         elements = []
 
         # First add the root node value
-        elements.append(self.data)
+        elements.append(node.data)
 
         # vist Left node, then recursively find for the furthest left leaf node,
         # then add in the array
-        if self.left:
-            elements += self.left.preOrderTraversal()
+        if node.left:
+            elements += self.preOrderTraversal(node.left)
 
         # vist right  node, then recursively find for the furthest left leaf node,
         # then add in the array
-        if self.right:
-            elements += self.right.preOrderTraversal()
+        if node.right:
+            elements += self.preOrderTraversal(node.right)
 
         return elements
 
     # Using LRV to return an array of numbers/elements
-    def postOrderTraversal(self):
+    def postOrderTraversal(self, node):
         #Initialize empty array
         elements = []
 
         # vist Left node, then recursively find for the furthest left leaf node,
         # then add in the array
-        if self.left:
-            elements += self.left.postOrderTraversal()
+        if node.left:
+            elements += self.postOrderTraversal(node.left)
 
         # vist right  node, then recursively find for the furthest left leaf node,
         # then add in the array
-        if self.right:
-            elements += self.right.postOrderTraversal()
+        if node.right:
+            elements += self.postOrderTraversal(node.right)
 
         # Lastly add the root node value
-        elements.append(self.data)
+        elements.append(node.data)
 
         return elements
 
+    # Find the highest value of the BST, which is always on the right side of the tree,
+    # the last node on the right is always the biggest
+    def findMax(self, node):
 
+        if node.right is None:
+            return node.data
 
+        return self.findMax(node.right)
+
+    # Find the lowest value of the BST, which is always on the left side of the tree,
+    # the last node on the left is always the smallest
+    def findMin(self, node):
+
+        if node.left is None:
+            return node.data
+
+        return self.findMin(node.left)
 
 if __name__ == "__main__":
 
@@ -133,6 +147,6 @@ if __name__ == "__main__":
     for i in range(1, len(numbers)):
         numbers_tree.addChild(root, numbers[i])
 
-    print(numbers_tree.search(root, 18))
+    print(numbers_tree.findMin(root))
 
 
